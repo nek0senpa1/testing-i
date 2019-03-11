d5b2e2aa0d31bf9adb71b5a5b57037b25975ffea
amberm <nekosenpai@gmail.com>
2019-03-11 14:01:58 -0600

    got 2 of 3 tests working correctly

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,54 @@
-const enhancer = require('./enhancer.js');
+const {succeed, fail, repair, get} = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', ()=> {
+
+    test ('is working?', () => {
+        expect(true).toBe(true);
+    })
+
+
+
+    describe('repair()', () =>{
+
+        it ('repairs to durability 100', () => {
+            expect(repair(
+                {
+                    name: 'Sword',
+                    enhancement: 15,
+                    durability: 90,
+                }
+            )).toEqual(
+                {
+                    name: 'Sword',
+                    enhancement: 15,
+                    durability: 100,
+                }
+            );
+        })
+
+    })
+
+    describe('succeed()', function weeeee() {
+
+        it('succeed at enhancing', () => {
+
+            expect(succeed(
+                {
+                    name: 'Sword',
+                    enhancement: 15,
+                    durability: 90,
+                }
+            )).toEqual (
+                {
+                    name: 'Sword',
+                    enhancement: 16,
+                    durability: 90,
+                }
+            )
+
+        })
+        
+    })
+
+})
